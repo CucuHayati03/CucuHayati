04d2115aa05c60c0c49fdb8ad9a1b02d2987e8b9
[Cucu <cucuhyti802@gmail.com>
2022-12-08 11:12:33 +0700

    commit ke dua

--- buttonn/parts/footer.js
+++ buttonn/parts/footer.js
@@ -33,6 +33,49 @@ export default function footer() {
                             </li>
                         </ul>
                     </div>
+                    <div className="col-2 mr-5">
+                    <h6 className="mt-2">Explore Us</h6>
+                        <ul classsName="LIst-group Lis-group-flush">
+                            <li classname="list-group-item">
+                                <Button type="link" href="/careers">
+                                   Our careers
+                                </Button>
+                            </li>
+                            <li classname="list-group-item">
+                                <Button type="link" href="/privacy">
+                                    Privacy
+                                </Button>
+                            </li>
+                            <li classname="list-group-item">
+                                <Button type="link" href="/terms">
+                                    Terms & Conditions
+                                </Button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-4">
+                    <h6 className="mt-2">Connect Us</h6>
+                        <ul classsName="LIst-group Lis-group-flush">
+                            <li classname="list-group-item">
+                                <Button isExternal type="link" href="mailto:support@sraycation.id">
+                                   Support@staycation.id
+                                </Button>
+                            </li>
+                            <li classname="list-group-item">
+                                <Button isexternal type="link" href="tel:+622122081996">
+                                    021 - 2200 - 1996
+                                </Button>
+                            </li>
+                            <li classname="list-group-item">
+                                <span>Staycation, Kemang, Jakarta</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div classname="row">
+                    <div className="col text-center copyrights">
+                        Copyright 2019 . All right reserved . Staycation
+                    </div>
                 </div>
             </div>
         </footer>
